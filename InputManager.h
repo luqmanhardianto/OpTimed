b1cbdd93384af9cd1091b6d4110f2b1cbbbe2377
luqman hardianto <luqmanhardianto@gmail.com>
2026-09-17 13:51:19 +0700

    create:contract interface for InputManager

--- InputManager.h
+++ InputManager.h
@@ -1,1 +1,23 @@
 #pragma once
+
+#include <Arduino.h>
+#include <stdint.h>
+
+#include "Config.h"
+#include "HardwareMap.h"
+
+class InputManager {
+private:
+  uint8_t pin;
+
+  bool lastInputState = HIGH;
+  bool stableinputState = HIGH;
+
+  unsigned long lastTimeStateChanged = 0;
+  unsigned long debounceTimeMs = INPUT_DEBOUNCE_MS;
+
+public:
+  void begin(DigitalInput input);
+  void readState();
+  bool isActive() const;
+};
