a66488e7d2be4711d9817fca9cbb7531efebf6c3
luqman hardianto <luqmanhardianto@gmail.com>
2026-09-24 11:18:33 +0700

    change type data to uint16

--- src/Config.h
+++ src/Config.h
@@ -4,9 +4,9 @@
 #define I2C_PIN_SCL A5
 
 namespace SystemTiming {
-constexpr uint8_t DEBOUNCE_MS = 100;
-constexpr uint8_t LONGPRESS_MS = 1000;
-constexpr uint8_t BLINK_MS = 500;
+constexpr uint16_t DEBOUNCE_MS = 100;
+constexpr uint16_t LONGPRESS_MS = 1000;
+constexpr uint16_t BLINK_MS = 500;
 }
 
 #define DS3231_ADDRESS 0x68
