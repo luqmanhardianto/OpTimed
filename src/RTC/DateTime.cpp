34f3ed60a5f6e8ddd60173f2d53a7ce69ac59c52
luqman hardianto <luqmanhardianto@gmail.com>
2026-09-24 09:04:12 +0700

    return LeapYear : true/false

---
+++ src/RTC/DateTime.cpp
@@ -0,0 +1,13 @@
+#include "DateTime.h"
+
+bool isLeapYear(uint16_t year) {
+  if ((year % 400) == 0) {
+    return true;
+  }
+
+  if ((year % 100) == 0) {
+    return false;
+  }
+
+  return (year % 4) == 0;
+}
