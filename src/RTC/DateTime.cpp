a8b015894aa6d76f6668c0230372140bf07ffed0
luqman hardianto <luqmanhardianto@gmail.com>
2026-09-24 09:08:55 +0700

    return uint8_t day number in month

--- src/RTC/DateTime.cpp
+++ src/RTC/DateTime.cpp
@@ -11,3 +11,48 @@ bool isLeapYear(uint16_t year) {
 
   return (year % 4) == 0;
 }
+
+uint8_t daysInMonth(uint16_t year, uint8_t month) {
+  switch (month) {
+    case 1:
+      return 31;
+
+    case 2:
+      return isLeapYear(year) ? 29 : 28;
+
+    case 3:
+      return 31;
+
+    case 4:
+      return 30;
+
+    case 5:
+      return 31;
+
+    case 6:
+      return 30;
+
+    case 7:
+      return 31;
+
+    case 8:
+      return 31;
+
+    case 9:
+      return 30;
+
+    case 10:
+      return 31;
+
+    case 11:
+      return 30;
+
+    case 12:
+      return 31;
+
+    default:
+      return 0;
+  }
+}
+
+bool isValidDAteTime(const DateTime &dateTime);
