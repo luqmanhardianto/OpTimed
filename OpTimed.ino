03839e68ebdd794d02663b7b148baa29670d6da5
luqman hardianto <luqmanhardianto@gmail.com>
2026-09-24 13:47:23 +0700

    test input pushbutton with output led

--- OpTimed.ino
+++ OpTimed.ino
@@ -1,22 +1,29 @@
 #include "src/HardwareMap.h"
 #include "src/Input/PushButton.h"
+#include "src/Output/Led.h"
 
 
 PushButton buttonPower;
+Led ledPower;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(115200);
   delay(500);
+
   buttonPower.begin(DigitalInput::BUTTON_POWER);
-  buttonPower.readState();
-  delay(100);
+  ledPower.begin(DigitalOutput::POWER_LED);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   buttonPower.readState();
+  if (buttonPower.isPressed()) {
+  ledPower.on();
+  } else {
+  ledPower.off();
+  }
+  Serial.print("btnPower:");
   Serial.println(buttonPower.isPressed());
-  delay(500);
-
+  delay(100);
 }
