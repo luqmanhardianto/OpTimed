8a52d4edc8d4bec5bcb97b310273650045037a99
luqman hardianto <luqmanhardianto@gmail.com>
2026-09-24 14:42:22 +0700

    check connection Wire

--- src/RTC/DS3231.cpp
+++ src/RTC/DS3231.cpp
@@ -16,4 +16,7 @@ bool DS3231::setSqw1Hz();
 bool DS3231::isOscillatorStopped();
 bool DS3231::clearOscillatorStopFlag();
 
-bool DS3231::isCOnnected();
+bool DS3231::isCOnnected() {
+  Wire.beginTransmission(I2C_ADDRESS);
+  return Wire.endTransmission() == 0;
+}
